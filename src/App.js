eee319d2a8ff4bca879c8429ec5a2a58eeed1ecd
thevioletmaniac <thevioletmaniac@gmail.com>
2021-06-17 19:37:41 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,84 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { useStoreon } from 'storeon/react';
 
+import Loader from './components/base/Loader';
+import { AdminRoute, PrivateRoute } from './components/base/Routes/';
+import Nav from './components/common/Nav';
+import Notifications from './components/common/Notifications';
+import Sidebar from './components/common/Sidebar';
 import Admin from './components/pages/Admin';
 import Error from './components/pages/Error';
 import Main from './components/pages/Main';
 import Profile from './components/pages/Profile';
 import SignIn from './components/pages/SignIn';
 import SignUp from './components/pages/SignUp';
+import { auth } from './config';
+import { getUserDoc } from './controllers/firebase/auth';
 
 const App = () => {
-  const [user, setUser] = useState({
-    name: 'Dashich',
-    isLogin: true,
-    isAdmin: false,
-  });
+  const { user, dispatch } = useStoreon('user');
+  const [isInitializing, setInitializing] = useState(true);
+  const [currentUser, setCurrentUser] = useState(null);
 
-  const PrivateRoute = ({ component: Component, isLogin, ...rest }) => {
-    return <Route {...rest}>{isLogin ? <Component /> : <SignIn />}</Route>;
+  const onAuthStateChanged = (user) => {
+    setCurrentUser(user);
+
+    if (isInitializing) setInitializing(false);
   };
 
-  const AdminRoute = ({ component: Component, isAdmin, ...rest }) => {
-    return <Route {...rest}>{isAdmin ? <Component /> : <Error message="Access denied" />}</Route>;
-  };
+  useEffect(() => {
+    const subscriber = auth.onAuthStateChanged(onAuthStateChanged);
 
-  return (
-    <div>
-      <Switch>
-        <Route path="/" exact>
-          <Main />
-        </Route>
-        <PrivateRoute path="/profile" isLogin={user.isLogin} component={Profile} />
-        <AdminRoute path="/admin" isAdmin={user.isAdmin} component={Admin} />
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-    </div>
+    return subscriber;
+  }, []);
+
+  useEffect(() => {
+    const checkUserRole = async (userId) => {
+      const result = await getUserDoc(userId);
+
+      if (result.isAdmin) {
+        return true;
+      }
+      return false;
+    };
+
+    if (currentUser) {
+      const isUserAdmin = checkUserRole(currentUser.uid);
+
+      const userInfo = {
+        isLogin: true,
+        displayName: currentUser.displayName,
+        email: currentUser.email,
+        isAdmin: isUserAdmin ? true : false,
+      };
+
+      dispatch('user/login', userInfo);
+    }
+  }, [currentUser]);
+
+  return isInitializing ? (
+    <Loader />
+  ) : (
+    <>
+      <div className="wrapper">
+        <Nav />
+        <div className="container">
+          <Sidebar />
+          <main className="content">
+            <Switch>
+              <PrivateRoute exact path="/" isLogin={user.isLogin} component={Main} />
+              <PrivateRoute path="/profile" isLogin={user.isLogin} component={Profile} />
+              <AdminRoute path="/admin" isLogin={user.isLogin} isAdmin={user.isAdmin} component={Admin} />
+              <Route path="/signin" component={SignIn} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="*" component={Error} />
+            </Switch>
+          </main>
+        </div>
+      </div>
+      <Notifications />
+    </>
   );
 };
 
