71d7fa386a4b36266f9990e59bca2251cf4b2da6
thevioletmaniac <thevioletmaniac@gmail.com>
2021-06-17 19:27:21 +0600

    Adjust Error page layout

--- src/components/pages/Error/Error.js
+++ src/components/pages/Error/Error.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Text from './../../base/Text';
+
 import './style.scss';
 
 const Error = ({ message = 'Something went wrong' }) => {
   return (
     <div>
-      <h1>{message}</h1>
+      <h1>Oops!</h1>
+      <Text hasGap>{message}</Text>
     </div>
   );
 };
