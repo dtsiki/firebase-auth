97e8e2c014edeeca1531ea9a5f54eba34559269f
thevioletmaniac <thevioletmaniac@gmail.com>
2021-06-17 19:32:52 +0600

    Update main page layout

--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -1,16 +1,14 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 
-import './style.scss';
+import Text from './../../base/Text';
 
 const Main = () => {
   return (
     <div>
       <h1>Main</h1>
+      <Text hasGap>How are you doing? 😜</Text>
     </div>
   );
 };
-
-Main.propTypes = {};
 
 export default Main;
