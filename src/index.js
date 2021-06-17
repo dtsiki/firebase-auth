59644eaa8e87b25fdee29ad098aaa9a121ded262
thevioletmaniac <thevioletmaniac@gmail.com>
2021-06-17 19:23:37 +0600

    Add store wrapper

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { StoreContext } from 'storeon/react';
 
 import App from './App';
+import { store } from './store';
 
 import './styles/main.scss';
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <StoreContext.Provider value={store}>
+        <App />
+      </StoreContext.Provider>
     </React.StrictMode>
   </BrowserRouter>,
   document.getElementById('app')
